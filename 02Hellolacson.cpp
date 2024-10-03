9f39d42c3ac72f2c0c59824ee025e2e7f532e1cb
defnotver <davealmira8@gmail.com>
2024-10-03 10:08:32 +0800

    Add files via upload

--- 02Hellolacson.cpp
+++ 02Hellolacson.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World" << endl;
+	cout << "SCHOOL\t\t\t:OLLCF" << endl;
+	cout << "DEPARTMENT\t\t:CCS" << endl;
+	cout << "COURSE & YEAR LEVEL\t:BSIT 1st year" << endl;
+	cout << "NAME\t\t\t:Dave Iver Lacson" << endl;
+	cout << "ANDRESS\t\t\t:P-1 Luklukan Sur Jose Panganiban" << endl;
+
 	return 0;
 	
 
